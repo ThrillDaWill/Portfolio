23bbd22da7206a0d484a39492cf74d92c673e89d
Brianna Workman <107228348+briannaworkman@users.noreply.github.com>
2023-09-18 22:02:49 -0400

    initial commit + cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+# File Structure
---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from '@/components/Navigation/Navigation/Navigation'
+import './App.scss'
+
+export default function Home() {
+  return (
+    <main className='app'>
+      <Navigation />
+    </main>
+  )
+}
