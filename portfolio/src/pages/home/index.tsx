2d81e5b8ea0ea753572f95088aa9efb37a42f8f3
Brianna Workman <107228348+briannaworkman@users.noreply.github.com>
2023-09-29 21:14:25 -0400

    Sets up home page

---
+++ portfolio/src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../app/App.scss';
+
+export default function Home() {
+    return (
+      <main className='app'>
+        <div>I am the home page!</div>
+      </main>
+    )
+  }
