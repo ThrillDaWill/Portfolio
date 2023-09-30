3b4a65826d9daaec6be1e823dee4bc88a4dd7e81
Brianna Workman <107228348+briannaworkman@users.noreply.github.com>
2023-09-29 21:14:15 -0400

    Sets up and styles welcome page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,10 +1,18 @@
-import Navigation from '@/components/Navigation/Navigation/Navigation'
+import Link from 'next/link'
 import './App.scss'
 
-export default function Home() {
+export default function Welcome() {
   return (
     <main className='app'>
-      <Navigation />
+      <div className='container'>
+        <div className='title'>HI!</div>
+        <div className='description'>
+          <h2 className='subtitle'>My name is William Byrd. Welcome to my personal website.</h2>
+          <p className='p1'>I have created this website to feel like a game/sci-fi interface. All text inside tries to reflect this.</p>
+          <p className='p2'>You will find ‘achievements’ or ‘quests’ that show the progress in my professional life and are related to what I am working on.</p>
+          <Link className='button' href="/home">Enter The System</Link>
+        </div>
+      </div>
     </main>
   )
 }
